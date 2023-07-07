3cdd8da422feb34e60f72967c6d41c08137bf004
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-07 13:45:45 +0300

    Update diff_json.py
    add test with expected_file

--- tests/diff_json.py
+++ tests/diff_json.py
@@ -3,16 +3,10 @@ from gendiff.generate_diff import generate_diff as gdiff
 
 _FILE1 = 'tests/fixtures/file1.json'
 _FILE2 = 'tests/fixtures/file2.json'
-_EXPECTED = '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+_EXPECTED = 'tests/fixtures/expected_json.txt'
 
 def test_parse_json():
     result = gdiff(_FILE1, _FILE2)
-    print('tut', result)
-    assert result == _EXPECTED
+    with open(_EXPECTED) as f:
+      report = f.read()
+    assert result == report
