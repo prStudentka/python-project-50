aa216a4470ac583d10839713a11ae526770eb378
prStudentka <bk_nvk@mail.ru>
2023-07-07 14:10:47 +0000

    fix lint cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -3,7 +3,8 @@ import argparse
 
 def start_diff(module):
     parser = argparse.ArgumentParser(
-            description='Compares two configuration files and shows a difference.')
+       description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
