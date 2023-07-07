d79e53e7dced17081a7f78085affbb74745a0454
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-07 13:07:20 +0300

    update test

--- tests/diff_json.py
+++ tests/diff_json.py
@@ -1,9 +1,17 @@
 import pytest
-
+from gendiff.generate_diff import generate_diff as gdiff
 
 _FILE1 = 'fixtures/file1.json'
 _FILE2 = 'fixtures/file2.json'
-
+_EXPECTED = '''
+{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
 
 def test_parse_json():
-    pass
+    assert gdiff(_FILE1, _FILE2) == _EXPECTED
