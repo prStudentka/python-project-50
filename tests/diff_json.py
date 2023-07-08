a9fbc5e6cdf948888ad9adb1172710a84b7e3fc7
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-08 22:44:33 +0300

    Update diff_json.py

--- tests/diff_json.py
+++ tests/diff_json.py
@@ -1,6 +1,8 @@
 import pytest
 from gendiff.generate_diff import generate_diff as gdiff
 from gendiff.parser_end import parser_file
+from gendiff.cli import get_loader
+
 
 _FILE1 = 'tests/fixtures/file1.json'
 _FILE2 = 'tests/fixtures/file2.json'
@@ -9,11 +11,8 @@ _FILE2_YML = 'tests/fixtures/file2.yaml'
 _EXPECTED = 'tests/fixtures/expected_json.txt'
 
 
-def test_parser_end():
-    assert parser_file('tests/fixtures/expected_json.txt') == False
-
 def test_parse_json():
-    result = gdiff(_FILE1, _FILE2)
+    result = gdiff(get_loader(_FILE1), get_loader(_FILE2))
     with open(_EXPECTED, 'r') as f:
       report = f.read()
     assert result == report
