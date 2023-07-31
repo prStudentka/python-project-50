e2d58d2b4e0a9e8bb99f3f3b7c23a1504139aa46
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-07-31 08:50:51 +0300

    lint fix generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,22 +1,6 @@
 from difference import differ as get_difference
 from formater.stylish import formating
 
-def differ(data1, data2):
-    def walk(key):
-        if key not in data1:
-            return choose_fill(key, data2[key], 'add')
-        elif key not in data2:
-            return choose_fill(key, data1[key], 'remove')
-        elif data1[key] == data2[key]:
-            return mkvalue(key, data1[key])
-        if isinstance(data1[key],dict) and isinstance(data2[key], dict):
-            return mknest(key, differ(data1[key], data2[key]))
-        else:
-            change_dict = choose_fill(key, data1[key], 'remove')
-            change_dict2 = choose_fill(key, data2[key], 'add')
-            return mknest(key, [change_dict, change_dict2], 'change')
-    return list(map(walk, sorted((data1.keys() | data2.keys()))))
-
 
 def generate_diff(file1, file2, format='stylish'):
     # diff - разница словарь
