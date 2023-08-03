46cc2d95fb9396a4ff2643c7e16e559899dc0ea0
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-08-03 09:19:03 +0300

    fix lint generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,12 +3,12 @@ from .formater.stylish import formating as stylish
 from .formater.plain import formating as plain
 
 
-_DICT_FORMATS ={
+_DICT_FORMATS = {
     'stylish': stylish,
     'plain': plain
 }
-    
+
 
 def generate_diff(file1, file2, format='stylish'):
     dict_diff = get_difference(file1, file2)
-    return __DICT_FORMATS.get(format, 'Wrong format')(dict_diff)
+    return _DICT_FORMATS.get(format, 'Wrong format')(dict_diff)
