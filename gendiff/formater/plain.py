befbb550d16adc164ae5e811c7f778913aceba58
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-08-03 13:25:48 +0300

    fix lint plain

--- gendiff/formater/plain.py
+++ gendiff/formater/plain.py
@@ -25,15 +25,15 @@ def formating(diffs, path=""):
         head = f"{path}{item['name']}"
         level = f"Property '{head}' was "
         if item['meta'] == 'added':
-            value = f"{level + item['meta']} with value: {convert(item)}"
+            value = f"{level}{item['meta']} with value: {convert(item)}"
             lines.append(value)
         if item['meta'] == 'removed':
-            lines.append(level + item['meta'])
-        if is_nest(item):
-            if item['meta'] == 'updated':
-                value = list(map(convert, item['children']))
-                lines.append(f"{level + item['meta']}. From {(value[0])} to {value[1]}")
-            elif item['meta'] == 'unchanged':
-                value = formating(item['children'], f"{head}.")
-                lines.append(f"{value}")
+            value = f"{level}{item['meta']}"
+            lines.append(value)
+        if item['meta'] == 'updated':
+            value = list(map(convert, item['children']))
+            lines.append(f"{level}{item['meta']}. From {(value[0])} to {value[1]}")
+        if is_nest(item) and item['meta'] == 'unchanged':
+            value = formating(item['children'], f"{head}.")
+            lines.append(f"{value}")
     return '\n'.join(lines)
