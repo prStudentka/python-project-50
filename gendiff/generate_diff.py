a3ef410f9581cab1f48491ab7b76129fad88f00c
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-08-03 09:00:16 +0300

    Update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,7 +1,14 @@
 from gendiff.difference import differ as get_difference
-from gendiff.formater.stylish import formating
+from .formater.stylish import formating as stylish
+from .formater.plain import formating as plain
 
+
+_DICT_FORMATS ={
+    'stylish': stylish,
+    'plain': plain
+}
+    
 
 def generate_diff(file1, file2, format='stylish'):
     dict_diff = get_difference(file1, file2)
-    return formating(dict_diff)
+    return __DICT_FORMATS.get(format, 'Wrong format')(dict_diff)
