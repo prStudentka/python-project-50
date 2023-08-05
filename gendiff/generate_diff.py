a785f575ad78ddb005376095afd6f73445416476
prStudentka <bk_nvk@mail.ru>
2023-08-05 18:45:43 +0000

    change generate_diff for test hexlet

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,7 +2,9 @@ from gendiff.difference import differ as get_difference
 from .formater.stylish import formating as stylish
 from .formater.plain import formating as plain
 from .formater.to_json import formating as js
-from gendiff.cli import start_cli
+import argparse
+import pathlib as path
+import gendiff.parser_end as pe
 
 
 _DICT_FORMATS = {
@@ -12,12 +14,37 @@ _DICT_FORMATS = {
 }
 
 
+def get_args():
+    parser = argparse.ArgumentParser(
+       description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', default='stylish',
+                        choices=['stylish', 'plain', 'json'],
+                        help='set format of output, default="stylish"')
+    return parser.parse_args()
+
+
+def get_data(file):
+    with open(file, 'r') as fi:
+        loader = pe.parser_suffix((path.PurePath(file).suffix)[1:])
+        if loader is None:
+            raise Exception('extention error')
+        return loader(fi)
+
+
 def start_diff():
-    file1, file2, _format = start_cli()
-    diff = generate_diff(file1, file2, _format)
+    arg = get_args()
+    diff = generate_diff(arg.first_file, arg.second_file, arg.format)
     print(diff)
 
 
-def generate_diff(file1, file2, format='stylish'):
-    dict_diff = get_difference(file1, file2)
-    return _DICT_FORMATS.get(format, 'Wrong format')(dict_diff)
+def generate_diff(path1, path2, format='stylish'):
+    if path.Path(path1).exists() and path.Path(path2).exists():
+        file1 = get_data(path1)
+        file2 = get_data(path2)
+        dict_diff = get_difference(file1, file2)
+        return _DICT_FORMATS.get(format, 'Wrong format')(dict_diff)
+    else:
+        raise FileNotFoundError('one of files not found')
