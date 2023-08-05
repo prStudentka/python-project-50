18978f143b522b88a9ceceded651625403d9f419
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-08-05 17:36:21 +0300

    Update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,9 @@
 #!user/bin/env python3
 from gendiff import generate_diff
-from gendiff.cli import start_diff
 
 
 def main():
-    start_diff(generate_diff)
+    generate_diff()
 
 
 if __name__ == '__main__':
