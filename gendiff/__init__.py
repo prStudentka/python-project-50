19c6167dd247ad2fcce1b33f5f41d4bd82e78ff2
prStudentka <bk_nvk@mail.ru>
2023-08-05 17:33:21 +0000

    add dev pyyaml

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from .generate_diff import generate_diff
 __all__ = [generate_diff]
-=======
-from .generate_diff import generate_diff
-__all__ = [generate_diff]
->>>>>>> ae3a2dab65dbfe90474c02c4277cbe106a0bd84f
