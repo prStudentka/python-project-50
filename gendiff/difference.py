0f51ab3e0cbb1d57e1bdb5a15b7671e43f144822
prStudentka <135240986+prStudentka@users.noreply.github.com>
2023-08-17 18:30:48 +0300

    Update difference.py

--- gendiff/difference.py
+++ gendiff/difference.py
@@ -1,4 +1,4 @@
-def mkvalue(key, value, meta='unchanged'):
+def make_value(key, value, meta='unchanged'):
     return {
         'name': key,
         'value': value,
@@ -7,7 +7,7 @@ def mkvalue(key, value, meta='unchanged'):
     }
 
 
-def mknest(key, children, meta='unchanged'):
+def make_nest(key, children, meta='unchanged'):
     return {
         'name': key,
         'children': children,
@@ -18,11 +18,11 @@ def mknest(key, children, meta='unchanged'):
 
 def choose_fill(key, item, meta='unchanged'):
     if type(item) is not dict:
-        return mkvalue(key, item, meta)
+        return make_value(key, item, meta)
     children = []
     for key_item in item.keys():
         children.append(choose_fill(key_item, item[key_item]))
-    return mknest(key, children, meta)
+    return make_nest(key, children, meta)
 
 
 def differ(data1, data2):
@@ -32,11 +32,11 @@ def differ(data1, data2):
         elif key not in data2:
             return choose_fill(key, data1[key], 'removed')
         elif data1[key] == data2[key]:
-            return mkvalue(key, data1[key])
+            return make_value(key, data1[key])
         if isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            return mknest(key, differ(data1[key], data2[key]))
+            return make_nest(key, differ(data1[key], data2[key]))
         else:
             change_dict = choose_fill(key, data1[key], 'removed')
             change_dict2 = choose_fill(key, data2[key], 'added')
-            return mknest(key, [change_dict, change_dict2], 'updated')
+            return make_nest(key, [change_dict, change_dict2], 'updated')
     return list(map(walk, sorted((data1.keys() | data2.keys()))))
